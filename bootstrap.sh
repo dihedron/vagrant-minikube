3c407f94bd14ff6a84b299b2278c4890b33c36a3
Andrea Funtò <dihedron.dev@gmail.com>
2018-03-26 23:30:26 +0200

    Minikube installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,56 +1,52 @@
 #!/usr/bin/env bash
 
-KVM_DRIVER_VERSION=v0.10.0
 MINIKUBE_VERSION=v0.25.2
 
-#
-# INSTALL PREREQUISITES (CURL AND LIBVIRT/KVM)
-#
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y curl qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+echo "running as $(whoami)..."
 
 #
-# HACK: CHANGE OWNERSHIP OF LIBVIRT
+# UPDATE SYSTEM AND INSTALL BASE PACKAGES
 #
-sudo chown vagrant /var/run/libvirt
+apt-get update -y
+apt-get dist-upgrade -y
+apt-get install -y curl apt-transport-https ca-certificates software-properties-common
+
+#
+# INSTALL PREREQUISITES (DOCKER CE)
+#
+echo -n "installing docker ce..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-get install -y docker-ce
+groupadd docker
+usermod -aG docker vagrant
+newgrp docker
+echo "DONE!"
+
+echo "user vagrant belongs to $(groups vagrant)"
 
 #
 # DOWNLOAD AND INSTALL KUBECTL
 #
 echo -n "downloading and installing kubectl..."
-curl -LsO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && sudo mv kubectl /usr/local/bin/ && sudo chmod 755 /usr/local/bin/kubectl
+curl -LsO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && mv kubectl /usr/local/bin/ && chmod 755 /usr/local/bin/kubectl
 echo "DONE!"
 
 #
 # DOWNLOAD AND INSTALL MINIKUBE
 #
 echo -n "downloading and installing minikube..."
-curl -Lso minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ && sudo chmod 755 /usr/local/bin/minikube
+curl -Lso minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-linux-amd64 && mv minikube /usr/local/bin/ && chmod 755 /usr/local/bin/minikube
+minikube start --vm-driver none
 echo "DONE!"
 
-#
-# CONFIGURE VAGRANT USER TO LOAD KVM/KVM2 DRIVERS 
-#
-echo -n "configuring user rights..."
-sudo usermod -a -G libvirtd vagrant
-newgrp libvirtd
-echo "DONE!"
+#echo -n "installing minikube as daemon..."
+#cp /vagrant/minikube.service /etc/systemd/system/
+#cp /vagrant/minikube.sh /usr/local/bin && chmod 755 /usr/local/bin/minikube.sh 
+#systemctl daemon-reload
+#systemctl enable minikube.service
+#systemctl start minikube.service
+#echo "DONE!"
 
-echo "user $(whoami) belongs to $(groups)"
-
-#
-# DOWNLOAD INSTALL AND CONFIGURE KVM2 DRIVER
-#
-echo -n "downloading and installing KVM2 driver..."
-curl -LsO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && sudo mv docker-machine-driver-kvm2 /usr/local/bin/ && sudo chmod 755 /usr/local/bin/docker-machine-driver-kvm2
-echo "DONE!"
-
-#
-# DOWNLOAD INSTALL AND CONFIGURE KVM DRIVER
-#
-echo -n "downloading and installing KVM driver..."
-curl -Lso docker-machine-driver-kvm https://github.com/dhiltgen/docker-machine-kvm/releases/download/${KVM_DRIVER_VERSION}/docker-machine-driver-kvm-ubuntu16.04 && sudo mv docker-machine-driver-kvm /usr/local/bin/ && sudo chmod 755 /usr/local/bin/docker-machine-driver-kvm
-echo "DONE!"
-
-echo "You can now run 'minikube start --vm-driver [kvm|kvm2]'"
+#echo "You can now run 'sudo minikube start --vm-driver none'"
---
+++ minikube.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/usr/local/bin/minikube start --vm-driver none
+
